0a9c6fb44038fd39264b2d7324dac01be28c1900
Dan Korostelev <nadako@gmail.com>
2018-01-26 03:29:53 +0300

    stupid parser

--- src/Main.hx
+++ src/Main.hx
@@ -1,8 +1,10 @@
 class Main {
 	static function main() {
+		haxe.Log.trace = haxe.Log.trace;
 		var scanner = new Scanner(sys.io.File.getContent("Main.mew"));
 		var head = scanner.scan();
 		var parser = new Parser(head);
-		parser.parse();
+		var result = parser.parse();
+		trace(result);
 	}
 }
--- src/Parser.hx
+++ src/Parser.hx
@@ -1,18 +1,83 @@
 class Parser {
 	var head:Token;
-	var lead:Token;
-	var trail:Token;
+	var trivia:Array<Token>;
 
 	public function new(head) {
 		this.head = head;
+		this.trivia = [];
 	}
 
 	public function parse() {
-		var token = head;
-		while (token != null) {
-			trace(token.kind, haxe.Json.stringify(token.text));
-			token = token.next;
+		switch advance() {
+			case {kind: TkIdent, text: "fun"}:
+				return parseFunction();
+			case _:
+				return null;
 		}
+	}
+
+	function parseFunction():SyntaxFunction {
+		var funToken = consume();
+
+		var nameToken = expect(t -> t.kind == TkIdent);
+
+		var openParenToken = expect(t -> t.kind == TkParenOpen);
+		var closeParenToken = expect(t -> t.kind == TkParenClose);
+
+		var node = new SyntaxFunction();
+		node.funToken = funToken;
+		node.nameToken = nameToken;
+		node.openParenToken = openParenToken;
+		node.closeParenToken = closeParenToken;
+		return node;
+	}
+
+	function expect(check):TokenInfo {
+		var token = advance();
+		return if (check(token)) consume() else null;
+	}
+
+	function advance():Token {
+		while (true) {
+			switch head.kind {
+				case TkWhitespace | TkNewline | TkLineComment | TkBlockComment | TkDocComment | TkInvalid:
+					trivia.push(head);
+				case _:
+					break;
+			}
+			head = head.next;
+		}
+		return head;
+	}
+
+	function consume():TokenInfo {
+		var info = new TokenInfo();
+		info.token = head;
+		info.leadTrivia = trivia;
+
+		trivia = [];
+		head = head.next;
+
+		info.trailTrivia = consumeTrailTrivia();
+		return info;
+	}
+
+	function consumeTrailTrivia():Array<Token> {
+		var result = [];
+		while (true) {
+			switch head.kind {
+				case TkWhitespace | TkLineComment | TkBlockComment | TkDocComment | TkInvalid:
+					result.push(head);
+					head = head.next;
+				case TkNewline:
+					result.push(head);
+					head = head.next;
+					break;
+				case _:
+					break;
+			}
+		}
+		return result;
 	}
 }
 
@@ -20,5 +85,13 @@ class TokenInfo {
 	public var token:Token;
 	public var leadTrivia:Array<Token>;
 	public var trailTrivia:Array<Token>;
+	public function new() {}
+}
+
+class SyntaxFunction {
+	public var funToken:TokenInfo;
+	public var nameToken:TokenInfo;
+	public var openParenToken:TokenInfo;
+	public var closeParenToken:TokenInfo;
 	public function new() {}
 }
